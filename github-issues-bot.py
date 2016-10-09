72e553a0dfe54ead9ff53512726f332afab75275
Martin Melka <melkamar@fit.cvut.cz>
2016-10-09 23:11:55 +0200

    Add comments, rewrite rules

--- github-issues-bot.py
+++ github-issues-bot.py
@@ -34,14 +34,14 @@ _____________________________________________
 class Rule:
     def __init__(self, regex, label):
         self.label = label.strip()
-        self.regex = re.compile(regex)
+        self.regex = re.compile(regex, re.IGNORECASE)
 
     def __str__(self, *args, **kwargs):
         return 'RULE: {}   --->   {}'.format(self.regex, self.label)
 
     def check_fits(self, text):
         # logger.debug("  ## check_fits [{}] and reg [{}]".format(text, self.regex))
-        if re.match(self.regex, text):
+        if re.search(self.regex, text):
             # logger.debug("  ##    --> yeah, fits.")
             return self.label
         else:
@@ -65,13 +65,10 @@ class Issue:
         self.reponame = reponame
 
     def __str__(self):
-        return 'Number #{}, Title: [{}], Body: [{}], URL: {}, ' \
-               'Open: {}, Labels: {}'.format(self.number,
-                                             self.title,
-                                             self.body,
-                                             self.url,
-                                             self.state_open,
-                                             self.labels)
+        return 'Number #{}, Title: [{}], Body: [{}], URL: {}'.format(self.number,
+                                                                     self.title,
+                                                                     self.body,
+                                                                     self.url)
 
     def has_labels(self):
         return len(self.labels) > 0
@@ -100,12 +97,21 @@ class Issue:
 
 
 def init_session(token):
+    """
+    Create an authorized session object with GitHub.
+    :param token: Auth token for GitHub.
+    :return: Requests session object.
+    """
     global github_session
     github_session = requests.Session()
     github_session.headers.update({'Authorization': 'token ' + token, 'User-Agent': 'Python'})
 
 
 def init_rules(filename):
+    """
+    Initialize global rules variable with rules defined in a file.
+    :param filename: File containing the rules.
+    """
     global rules
     rules = []
 
@@ -129,8 +135,8 @@ def init_rules(filename):
 def process_issues(token, repository, default_label, skip_labelled, process_comments, process_closed_issues,
                    process_title, remove_current):
     """
-    Main handling logic of the robot.
-    :return:
+    Main handling logic of the robot. Process issues in a given repository with the given settings.
+    Parameters correspond to command-line arguments.
     """
 
     init_session(token)
@@ -143,33 +149,41 @@ def process_issues(token, repository, default_label, skip_labelled, process_comm
     issues = fetch_issues(repository, state_param)
 
     for issue in issues:
-        logger.debug("ISSUE: {}".format(issue))
+        logger.debug("Issue: {}".format(issue))
 
         if skip_labelled and issue.has_labels():
-            logger.debug("  -> Skipping because it has labels and skip_labelled is True.")
+            logger.info("  -> Skipping issue {} because it has labels and skip_labelled is True.".format(issue))
             continue
 
         process_issue(issue, default_label, process_comments, process_title, remove_current)
 
 
 def apply_labels(issue, labels, remove_current):
+    """
+    Apply given labels to an issue on GitHub.
+    :param issue: Issue object to which to apply labels.
+    :param labels: List of labels (strings) to apply.
+    :param remove_current: If true, all current labels on the issue will be removed.
+    """
+
     if not labels:
         return
 
-    logger.info("Applying labels {} to issue {}.".format(labels, issue))
-
     patchurl = edit_issue_url.format(issue.reponame, issue.number)
 
-    if not remove_current:
-        # Init data with the current labels - we do not want to overwrite any
-        data = {
-            "labels": list(labels)
-        }
-    else:
-        data = []
+    logger.info("Applying labels {} to issue {}. ".format(labels, issue))
 
-    for label in issue.labels:
-        data['labels'].append(label['name'])
+    # Init data with the current labels - we do not want to overwrite any
+    data = {
+        "labels": list(labels)
+    }
+
+    if not remove_current:
+        logger.info("Keeping original labels: {}".format(issue.labels))
+        for label in issue.labels:
+            data['labels'].append(label['name'])
+    else:
+        logger.warn("Removing original labels from issue {} (Original labels: {})".format(issue, issue.labels))
 
     logger.debug("  Sending PATCH to {}. Data: {}".format(patchurl, data))
 
@@ -182,6 +196,15 @@ def apply_labels(issue, labels, remove_current):
 
 
 def process_issue(issue, default_label, process_comments, process_title, remove_current):
+    """
+    Handle rule matching and label adding on a given issue.
+    :param issue:
+    :param default_label:
+    :param process_comments:
+    :param process_title:
+    :param remove_current:
+    :return:
+    """
     labels = []
     for rule in rules:
         logger.debug("  Checking rule {}".format(rule))
@@ -200,15 +223,23 @@ def process_issue(issue, default_label, process_comments, process_title, remove_
                     break
 
         if new_label:
+            logger.debug("    -> rule matches.")
             labels.append(new_label)
 
     if default_label and not labels:
+        logger.warning("No rule matches. Applying default label: {}".format(default_label))
         labels.append(default_label)
 
     apply_labels(issue, labels, remove_current)
 
 
 def fetch_issues(repository, state):
+    """
+    Fetches all issues in a repository with the given state.
+    :param repository:
+    :param state: Required state of issues. Allowed string values: all, open, closed.
+    :return: List of Issue objects.
+    """
     get_url = fetch_issues_url.format(repository, state)
     logger.info("Fetching issues: {}".format(get_url))
 
@@ -233,6 +264,11 @@ def fetch_issues(repository, state):
 
 
 def fetch_comments(issue):
+    """
+    Fetches text of all comments of the given issue.
+    :param issue:
+    :return: List of comments.
+    """
     try:
         response = github_session.get(issue.comments_url)
         response.raise_for_status()
@@ -244,7 +280,7 @@ def fetch_comments(issue):
         return []
 
 
-def get_token(filename):
+def read_token(filename):
     config = configparser.ConfigParser()
     config.read(filename)
     return config['auth']['gittoken']
@@ -258,32 +294,33 @@ def log_num_to_level(value):
 
 
 @click.command()
-@click.argument('repository')
+@click.argument('repositories', nargs=-1)
 @click.option('-a', '--auth', default="auth.cfg", help="Authentication file. See auth.cfg.sample.")
-@click.option('-v', '--verbose', count=True)
+@click.option('-v', '--verbose', count=True,
+              help="Much verbosity. May be repeated multiple times. More v's, more info!")
 @click.option('-r', '--rules-file', 'rules_file', default="rules.cfg", help="File containing tagging rules.")
 @click.option('-i', '--interval', default=60, help="Interval of repository checking in seconds. Default is 60 seconds.")
 @click.option('-d', '--default-label', 'default_label', default="",
-              help="Label to apply to an issue if no other rule worked.")
-@click.option('--skip-labelled/--no-skip-labelled', 'skip_labelled', default=False,
-              help="If true, all issues that are already labelled will be skipped.")
+              help="Label to apply to an issue if no other rule applies. If empty, no label is applied. Defaults to no label.")
+@click.option('--skip-labelled/--no-skip-labelled', 'skip_labelled', default=True,
+              help="Should issues that are labelled already be skipped? Defaults to true.")
 @click.option('--comments/--no-comments', 'process_comments', default=True,
-              help="If true, comments will be also processed by the rules.")
+              help="Should comments be also matched against the rules? Defaults to true.")
 @click.option('--closed-issues/--no-closed-issues', 'process_closed_issues', default=False,
-              help="Should closed issues be still processed?")
+              help="Should closed issues be still processed? Defaults to false.")
 @click.option('--process-title/--no-process-title', 'process_title', default=True,
-              help="Should the title of the issue be matched against the rules as well?")
+              help="Should the title of the issue be matched against the rules as well? Defaults to true.")
 @click.option('--remove-current/--no-remove-current', 'remove_current', default=False,
-              help="Should the current labels on an issue be removed if a rule matches?")
-def main(repository, auth, verbose, rules_file, interval, default_label, skip_labelled, process_comments,
+              help="Should the current labels on an issue be removed if a rule matches? Defaults to false.")
+def main(repositories, auth, verbose, rules_file, interval, default_label, skip_labelled, process_comments,
          process_closed_issues, process_title, remove_current):
     """
-    REPOSITORY - Name of the repository to process, e.g. melkamar/mi-pyt-1
+    REPOSITORIES - Names of the repositories to process, e.g. melkamar/mi-pyt-1. Accepts whitespace-separated list.
     """
 
     logger.level = log_num_to_level(verbose)
     while True:
-        logger.warning(init_message.format(repository, logger.level, auth, rules_file, interval,
+        logger.warning(init_message.format(repositories, logger.level, auth, rules_file, interval,
                                            default_label,
                                            skip_labelled,
                                            process_comments, process_closed_issues, process_title,
@@ -292,10 +329,12 @@ def main(repository, auth, verbose, rules_file, interval, default_label, skip_la
         for rule in rules:
             logger.debug(rule)
 
-        process_issues(get_token(auth), repository, default_label, skip_labelled, process_comments,
-                       process_closed_issues,
-                       process_title, remove_current)
+        for repository in repositories:
+            process_issues(read_token(auth), repository, default_label, skip_labelled, process_comments,
+                           process_closed_issues,
+                           process_title, remove_current)
 
+        logger.info("Iteration done. Another will start in {} seconds.\n".format(interval))
         time.sleep(interval)
 
 
